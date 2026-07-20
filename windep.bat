99e410323bca6ed4fc83425b6999bf2fa9454a07
KeyFac <GLgele@outlook.com>
2026-07-21 01:19:40 +0800

    add zip packaging for release

--- windep.bat
+++ windep.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist WinReleaseBins mkdir WinReleaseBins
-copy /Y EmployeesDIR\build\Desktop_Qt_6_11_1_MinGW_64_bit_Release\EmployeesDIR.exe WinReleaseBins\EmployeesDIR.exe
+copy /Y EmployeesDIR\build\Desktop_Qt_6_11_1_MSVC2022_64bit_Release\EmployeesDIR.exe WinReleaseBins\EmployeesDIR.exe
 copy /Y EmployeesDIR\config.ini WinReleaseBins\
 if not exist WinReleaseBins\i18n mkdir WinReleaseBins\i18n
 copy /Y EmployeesDIR\EmployeesDIR_en_US.qm WinReleaseBins\i18n\
